149b073f980a12d2da32b8a3ba5884e1c15dcad7
rolandrevesz <fekete.revesz@gmail.com>
2022-05-14 01:23:16 +0200

    adding SignalRHub

--- MovieRentalApp.Endpoint/Controllers/RenterController.cs
+++ MovieRentalApp.Endpoint/Controllers/RenterController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using MovieRentalApp.Models;
 using MovieRentalApp.Logic;
+using Microsoft.AspNetCore.SignalR;
+using MovieRentalApp.Endpoint.Services;
 
 namespace MovieRentalApp.Endpoint.Controllers
 {
@@ -13,10 +15,12 @@ namespace MovieRentalApp.Endpoint.Controllers
     public class RenterController : ControllerBase
     {
         IRenterLogic logic;
+        IHubContext<SignalRHub> hub;
 
-        public RenterController(IRenterLogic logic)
+        public RenterController(IRenterLogic logic, IHubContext<SignalRHub> hub)
         {
             this.logic = logic;
+            this.hub = hub;
         }
 
         [HttpGet]
@@ -35,18 +39,22 @@ namespace MovieRentalApp.Endpoint.Controllers
         public void Create([FromBody] Renter value)
         {
             this.logic.Create(value);
+            this.hub.Clients.All.SendAsync("RenterCreated", value);
         }
 
         [HttpPut]
         public void Update([FromBody] Renter value)
         {
             this.logic.Update(value);
+            this.hub.Clients.All.SendAsync("RenterUpdated", value);
         }
 
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            var renterToDelete = this.logic.Read(id);
             this.logic.Delete(id);
+            this.hub.Clients.All.SendAsync("RenterDeleted", renterToDelete);
         }
     }
 }
