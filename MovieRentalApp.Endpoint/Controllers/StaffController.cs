2aad3805ad057b09a837d23be5ecfe6968b2db96
rolandrevesz <fekete.revesz@gmail.com>
2022-05-14 01:44:42 +0200

    adding SignalRHub messages

--- MovieRentalApp.Endpoint/Controllers/StaffController.cs
+++ MovieRentalApp.Endpoint/Controllers/StaffController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using MovieRentalApp.Models;
 using MovieRentalApp.Logic;
+using Microsoft.AspNetCore.SignalR;
+using MovieRentalApp.Endpoint.Services;
 
 namespace MovieRentalApp.Endpoint.Controllers
 {
@@ -13,10 +15,12 @@ namespace MovieRentalApp.Endpoint.Controllers
     public class StaffController : ControllerBase
     {
         IStaffLogic logic;
+        IHubContext<SignalRHub> hub;
 
-        public StaffController(IStaffLogic logic)
+        public StaffController(IStaffLogic logic, IHubContext<SignalRHub> hub)
         {
             this.logic = logic;
+            this.hub = hub;
         }
 
         [HttpGet]
@@ -35,18 +39,22 @@ namespace MovieRentalApp.Endpoint.Controllers
         public void Create([FromBody] Staff value)
         {
             this.logic.Create(value);
+            this.hub.Clients.All.SendAsync("StaffCreated", value);
         }
 
         [HttpPut]
         public void Update([FromBody] Staff value)
         {
             this.logic.Update(value);
+            this.hub.Clients.All.SendAsync("StaffUpdated", value);
         }
 
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            var staffToDelete = this.logic.Read(id);
             this.logic.Delete(id);
+            this.hub.Clients.All.SendAsync("StaffDeleted", staffToDelete);
         }
     }
 }
