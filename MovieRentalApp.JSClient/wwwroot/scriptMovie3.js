4a74090cef9c6adb8d44c297f3f94a2de67be88e
rolandrevesz <fekete.revesz@gmail.com>
2022-05-15 20:24:39 +0200

    creating the delete action

--- MovieRentalApp.JSClient/wwwroot/scriptMovie3.js
+++ MovieRentalApp.JSClient/wwwroot/scriptMovie3.js
@@ -1,14 +1,19 @@
 ﻿let movies = [];
 
-fetch('http://localhost:12229/movie')
-    .then(x => x.json())
-    .then(y => {
-        movies = y;
-        console.log(movies);
-        display();
-    });
+getdata();
+
+async function getdata() {
+    await fetch('http://localhost:12229/movie')
+        .then(x => x.json())
+        .then(y => {
+            movies = y;
+            console.log(movies);
+            display();
+        });
+}
 
 function display() {
+    document.getElementById('resultarea').innerHTML = "";
     movies.forEach(m => {
         document.getElementById('resultarea').innerHTML +=
             "<tr><td>" + m.movieId + "</td><td>" +
@@ -17,8 +22,24 @@ function display() {
             + m.distributor + "</td><td>"
             + m.category + "</td><td>"
             + m.length + "</td><td>"
-            + m.language + "</td></tr>";
+            + m.language + "</td><td>" +
+            `<button type="button" onclick="remove(${m.movieId})">Delete</button>`
+            + "</td></tr>";
     });
+}
+
+function remove(id) {
+    fetch('http://localhost:12229/movie/' + id, {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json', },
+        body: null
+    })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getdata();
+        })
+        .catch((error) => { console.error('Error:', error); });
 }
 
 function create() {
@@ -32,12 +53,12 @@ function create() {
         method: 'POST',
         headers: { 'Content-Type': 'application/json', },
         body: JSON.stringify(
-            { title: Title, distributor: Distributor, category: Category, length: Length, language: Language, rentId: RentId }),
+            { title: Title, distributor: Distributor, category: Category, length: Length, language: Language, rentId: RentId })
     })
         .then(response => response)
         .then(data => {
             console.log('Success:', data);
+            getdata();
         })
         .catch((error) => { console.error('Error:', error); });
-    display();
 }
